26eef0948167ae4fc9a9216936347c0c31daaa14
Pantal-pl <61602042+Pantal-pl@users.noreply.github.com>
2021-12-18 23:55:08 +0100

    new pages

--- projects/travel/dist/js/script.js
+++ projects/travel/dist/js/script.js
@@ -1,22 +1,4 @@
-const navbar = document.querySelector(".navbar");
 let triggerStart;
-
-const CTAButton = document.querySelector(".get-start")
-setInterval(()=>{
-    CTAButton.classList.toggle("jello-horizontal")
-},1500)
-gsap.registerPlugin(ScrollTrigger);
-if(window.innerWidth <=400) {
-  triggerStart = 240;
-}
-const sections = document.querySelectorAll('section');
-sections.forEach(section => {
-  gsap.fromTo(section.children, {y: '+=200', opacity: .30}, {y: 0, opacity: 1, duration: .5, ease: 'easeInOut', scrollTrigger: {
-  trigger: section,
-  start: `top ${triggerStart}%`,
-  scrub:1,
-}});  
-});
 
 const toggleMenuBtn = document.querySelector(".toggle-menu")
 const mobileMenu = document.querySelector(".navbar-mobile")
@@ -32,3 +14,20 @@ toggleMenuBtn.addEventListener("click", ()=>{
   mobileMenu.classList.toggle("navbar-mobile-active")
   document.querySelector("body").classList.toggle("navbar-mobile-active")
 })
+
+const CTAButton = document.querySelector(".get-start")
+setInterval(()=>{
+    CTAButton.classList.toggle("jello-horizontal")
+},1500)
+gsap.registerPlugin(ScrollTrigger);
+
+if(window.innerWidth <=400 ? triggerStart=220 : triggerStart=130);
+
+const sections = document.querySelectorAll('section');
+sections.forEach(section => {
+  gsap.fromTo(section.children, {y: '+=200', opacity: .30}, {y: 0, opacity: 1, duration: .5, ease: 'easeInOut', scrollTrigger: {
+  trigger: section,
+  start: `top ${triggerStart}%`,
+  scrub:1,
+}});  
+});
